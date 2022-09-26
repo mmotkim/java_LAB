25a4da15b493ac0559094d81406b0cf1debf9fd1
mmotkim <mmotkim2@gmail.com>
2022-09-27 04:25:21 +0700

    Update Worker.java

--- WorkerManager/src/workermanager/Worker.java
+++ WorkerManager/src/workermanager/Worker.java
@@ -9,5 +9,63 @@ package workermanager;
  * @author Mmotkim
  */
 public class Worker {
+
+    private int id;
+    private String name;
+    private int age;
+    private int salary;
+    private String location;
+
+    public Worker() {
+    }
+    
+    public Worker(int id, String name, int age, int salary, String location) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
     
 }
